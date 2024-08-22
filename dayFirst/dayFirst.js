1ace956fdcc2b13c2734f43a2d0b8ab89f396656
My Name <nikhilvaishnav364@gmail.com>
2024-08-22 18:52:24 +0530

    new code added

--- dayFirst/dayFirst.js
+++ dayFirst/dayFirst.js
@@ -1,2 +1,7 @@
-let a = 1;
-console.log("a");
+/*let a = 1;
+console.log("a");*/
+
+let firstName = "Nikhil";
+let secondName = "Vaishnan";
+let fullName = (firstName+" "+secondName);
+console.log(fullName);
