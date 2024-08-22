2a5c6fc99b3999b5c41f39f2ce6afe3dc5afb7ec
My Name <nikhilvaishnav364@gmail.com>
2024-08-22 19:27:33 +0530

    new line added

--- dayFirst/dayFirst.js
+++ dayFirst/dayFirst.js
@@ -6,6 +6,8 @@ let secondName = "Vaishnan";
 let fullName = (firstName+" "+secondName);
      console.log(fullName);*/
 
+// ---------------------------------------------------------------------------------------
+
 //Post increment age++ (first - Use then change)
 
 /*let age = 23;
@@ -14,6 +16,9 @@ console.log(newAge);*/
 
 //Pre increment ++age , (first - Change then use) 
 
-let age = 23;
+/*let age = 23;
 let newAge = ++age;
-console.log(age);
+console.log(age);*/
+
+// ---------------------------------------------------------------------------------------
+
