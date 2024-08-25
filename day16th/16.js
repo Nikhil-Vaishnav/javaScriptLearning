9c067efa2653771af9b94a28e21650ffbc7ac4e8
My Name <nikhilvaishnav364@gmail.com>
2024-08-25 16:29:46 +0530

    new code added

---
+++ day16th/16.js
@@ -0,0 +1,12 @@
+// string Methods
+//it trimes the white space  from the string.
+
+// trime method
+// let msg= "           hello";
+// msg.trim();
+// console.log(msg);
+
+
+// let password = prompt("Enter your password ");
+// let newPassword = password.trim();
+// console.log(newPassword);
