df1075079ced4dd2ffc9d3adc1914c876179b74c
My Name <nikhilvaishnav364@gmail.com>
2024-08-26 07:57:19 +0530

    new code added

--- day16th/16.js
+++ day16th/16.js
@@ -2,7 +2,7 @@
 //it trimes the white space  from the string.
 
 // trime method
-// let msg= "           hello";
+// let msg= "        hello";
 // msg.trim();
 // console.log(msg);
 
@@ -10,3 +10,20 @@
 // let password = prompt("Enter your password ");
 // let newPassword = password.trim();
 // console.log(newPassword);
+// ------------------------------------------------------------------------------------
+// toUpperCase Methods.
+// let name ="nikhil";
+// console.log(name.toUpperCase());
+
+// toLowerCase Method.
+// let name ="NIKHIL";
+// console.log(name.toLowerCase());
+
+// ------------------------------------------------------------------------------------
+
+// Methods with aruguments indexOf
+
+// let msg ="lLovecoding";
+// console.log(msg.indexOf("coding"));
+
+// ------------------------------------------------------------------------------------
