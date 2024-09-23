b3dab2e406135cf0bb4879a60009437a1c970509
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-09-23 22:19:49 +0530

    new file added

--- day16th/16.js
+++ day16th/16.js
@@ -310,11 +310,26 @@
 
 
 
+// loops
 
+// For loop
 
+// for(initialisation; conditions; updation){
+//     // do something
+// }
 
+// for (let i=1; i<=10; i++){
+//     console.log(i);
+// }
 
+// --------------------------------------------------------------------------------------------------------------------------------
+// document.getElementById("demo").innerHTML = "My First JavaScript";
 
+// print all 1 to 15 odd number.
+ 
+for (let i=1; i<=15; i+2){
+    console.log(i);
+}
 
 
 
---
+++ seventeenDay/seventeentth.js
@@ -0,0 +1,24 @@
+
+
+// loops
+
+// For loop
+
+// for(initialisation; conditions; updation){
+//     // do something
+// }
+
+// for (let i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// -----------------------------------------------------------------------------------------------
+//   print all odd numbers in till 15.
+// for (let i=1; i<=15; i=i+2){
+//     console.log(i);
+// }
+// ---------------------------------------------
+// print in revers direction all the odd from 15th.
+//  for(let i=15; i>=1; i=i-2){
+//     console.log(i);
+//  }
