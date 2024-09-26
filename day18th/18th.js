5467c060c6d8a3068996374300ef56983a49bc30
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-09-26 19:14:27 +0530

    new day added

---
+++ day18th/18th.js
@@ -0,0 +1,11 @@
+// Objects Liters: used to store keyed collections & complex entities.
+
+// student 
+//objects literals  
+
+// name  "Nikhil" p2
+// age   "23"  p1
+// key    value  
+
+
+// --------------------------------------------------------------------
