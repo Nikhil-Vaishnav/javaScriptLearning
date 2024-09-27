d43d114fa8784874565f927e39efb4e92ba7c856
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-09-27 19:02:10 +0530

    new game code added

--- day19th/19th.js
+++ day19th/19th.js
@@ -8,3 +8,14 @@
 // // calling of the function
 // MyName(); 
 // -------------------------------------------------------------------------------------
+
+// rollDice
+
+// function rollDice()
+// {
+//      let random=Math.floor(Math.random()*10)+1;
+//      console.log(Math.floor(Math.random()*6)+1)
+// };
+
+// rollDice();
+// -------------------------------------------------------------------------------------
