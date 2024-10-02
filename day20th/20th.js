5d80200ec74f9bae45ce9c96f9d2aaeb7f2b9648
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-10-02 17:52:50 +0530

    try and Catch added to the new code

--- day20th/20th.js
+++ day20th/20th.js
@@ -1,16 +1,55 @@
 // this:- This keyword is used refers to an object that is executing the 
 // current piece of code.  
 
-const student =
-{
-    name: "Nikhil",
-    age: 22,
-    English: 95,
-    Hindi: 98,
-    Maths: 40,
-    science: 70,
-    getAvg(){
-        let avg =(this.Hindi + this.Maths + this.science +this.English )/4
-        console.log(`${this.name} got ave marks=${avg}`);
-    }
-}
+// const student =
+// {
+//     name: "Nikhil",
+//     age: 22,
+//     English: 95,
+//     Hindi: 98,
+//     Maths: 40,
+//     science: 70,
+//     getAvg(){
+//         let avg =(this.Hindi + this.Maths + this.science +this.English )/4
+//         console.log(`${this.name} got ave marks=${avg}`);
+//     }
+// }
+// -------------------------------------------------------------------------------------
+
+// const student =
+// {
+//     name: "Nikhil",
+//     age: 22,
+//     English: 95,
+//     Hindi: 98,
+//     Maths: 40,
+//     science: 70,
+//     getAvg(){
+//         console.log(this);
+//         let avg =(this.Hindi + this.Maths + this.science +this.English )/4
+//         console.log(`${this.name} got ave marks=${avg}`);
+//     }
+// }
+// -------------------------------------------------------------------------------------
+// Try and Catch 
+// The Try statement allows you to define a bolck of code to be tested for errors while it is being executed.
+
+// The catch statement allows you to define a block of code to be executed, if an error occures in the try block.
+
+// syantax:
+// try { 
+//     console.log(a);
+// }catch{
+//     console.log("varible a doest't");
+// }
+// ---------------------------------------------------
+
+// console.log("Nikhil");
+// console.log("Vaishnav");
+// try{
+//     console.log(a);// a is not defin so it will genrate the error but we have used catch for this.
+// }catch{
+//     console.log("a is not defined in code");//this line will be print 
+// }
+// console.log("Nikhil Vaishnav");
+// -------------------------------------------------------------------------------------
