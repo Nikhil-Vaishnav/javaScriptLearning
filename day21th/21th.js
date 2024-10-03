846aa04cd7dc95734d1561a931a1a42d1560dab2
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-10-03 09:41:19 +0530

    new code added

---
+++ day21th/21th.js
@@ -0,0 +1,53 @@
+// Array Methods
+
+// 1.forEach
+// 2.map 
+// 3.filter
+// 4.some
+// 5.every
+// 6.reduce
+
+// --------------------------------------------------------------
+
+// 1.forEach
+
+// let arr = [1,2,3,4,5];
+
+// let print = function (el){
+//     console.log(el);
+// };
+
+// arr.forEach(print);
+
+// ---------------------------
+
+// Using arrow function
+// let arr = [1,2,3,4,5];
+
+// arr.forEach((el)=>{
+//     console.log(el);
+// })
+
+// ---------------------------
+
+// by adding objects
+
+// let arr = [
+//     {
+//         name: "Nikhil",
+//         age:  22,
+//     },
+//     {
+//         name: "Rehan",
+//         age:  20,
+//     },
+//     {
+//         name: "Omprakash",
+//         age:  22,
+//     },
+// ];
+
+// arr.forEach((student)=> {
+//     console.log(student.age);
+// })
+// --------------------------------------------------------------
