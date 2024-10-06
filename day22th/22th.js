5fe0c2f44868a392c81d142e898023f0526f6ba0
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-10-06 16:23:54 +0530

    many things added

---
+++ day22th/22th.js
@@ -0,0 +1,8 @@
+// DOM (Document Object Model)
+//The DOM represents a document with a logical tree.
+//It allows us to manipulate/change webpages content (HTML elements).
+
+// for accssing h1 heading we using- console.dir(document.all[7].innerText);
+// document.all[7].innerText = "Peter Parker";
+
+
