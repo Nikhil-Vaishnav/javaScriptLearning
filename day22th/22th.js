8c0c3e273c4799b98c7134fbb94694b455233057
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-10-06 19:10:56 +0530

    new code added

--- day22th/22th.js
+++ day22th/22th.js
@@ -12,12 +12,27 @@
 //by using their id.
 //getElementById - Return the element as an object or null(if not found)
 
-// synatx - document.getElementId(id);
+// synatx - document.getElementId("id");
 
 //--------------------------------------
 
 // by using their classe.
 //getElementByClassName- Return the element as an HTML Collection or Enpty collection (if not found).
 
+// how we can accsses the multipe class with same name 
+// document.getElementClassName("oldImg")[0];
 
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for(let i = 0; i < smallImages.length; i++){
+//     smallImages[i].src = "imagees/sp2.jpeg";
+//     console.log(`value of images no. ${i}is changed.`);
+// }
+
+//--------------------------------------
+// by using their TagsNames.
+// getElementByTagName - Return the element as an HTML Collection or Enpty collection (if not found).
+
+// same as Classname 
+//----------------------------------------------------------------------------
 
