87db86833d23b90892ad516d778126140d04b553
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-10-21 16:31:26 +0530

    onclick events added

--- day23rd/23rd.js
+++ day23rd/23rd.js
@@ -2,8 +2,39 @@
 
 // onclick(when an element is clicked)
 
-let btn = document.querySelector('button');
+// let btn = document.querySelectorAll('button');
 
-btn.onclick= function(){
-    console.log("the button was click");
-}
+// btn.onclick= function(){
+//     console.log("the button was click")
+// };
+
+// btn.onclick= function(){
+//     alert("the button was click")
+// };
+// -------------------------------------------------------------------------------
+//  for the many buttons at the same time by using querySelectorAll.
+// let btn = document.querySelectorAll('button');
+
+// for (btn of btn){
+//     btn.onclick=sayhello;
+
+// }
+// function sayhello(){
+//     alert("hello"); 
+// }
+// btn.onclick=sayhello;
+// ------------------------------------------------- ------------------------------
+
+// onmouseenter(when mouse enters an elemnt)
+
+// let btn = document.querySelectorAll('button');
+
+// for (btn of btn){
+//     btn.onclick=sayhello;
+//     btn.onmouseenter = sayhello;
+// }
+// function sayhello(){
+//     console.log("hello"); 
+// }
+// btn.onclick=sayhello;
+// ------------------------------------------------- ------------------------------
