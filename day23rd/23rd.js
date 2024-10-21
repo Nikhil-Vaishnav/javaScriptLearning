cb178fc2f1e2a471c72e1873b11b37d33fa676f7
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-10-21 08:24:19 +0530

    dom added

--- day23rd/23rd.js
+++ day23rd/23rd.js
@@ -1,1 +1,9 @@
 // DOM Events-Events are signals that something has occured.(input/actions).
+
+// onclick(when an element is clicked)
+
+let btn = document.querySelector('button');
+
+btn.onclick= function(){
+    console.log("the button was click");
+}
