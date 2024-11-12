776ec19b48f085240236d2df6616a172e522e7c6
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-11-12 11:21:55 +0530

    dog api fetched

--- day27th/27th.js
+++ day27th/27th.js
@@ -1,35 +1,5 @@
-
+//  cat fact api's
 // let url = "https://catfact.ninja/fact";
-let url2  = "https://dog.ceo/api/breeds/image/random";
-
-let btn = document.querySelector("button");
-btn.addEventListener("click", async () =>{
-    // console.log("button was clicked");
-    let link = await getImage();
-    // console.log(link);
-    
-    let img = document.querySelector("#result");
-    img.setAttribute("src", link);
-
-    // let p = document.querySelector("#result");
-    // p.innerText = ;
-
-});
-
-
-// using Axios 
-async function  getImage(){
-  try{
-    let res = await axios.get(url2);
-    // let data = await res.json();/*we do not want this line because it diretclty convert the json to java object */
-    // return res.data.fact;
-    return res.data.message;      
-  }catch(error){
-    // console.log("Error",error); 
-    return "No fact found"
-  }
-}
-
 // let btn = document.querySelector("button");
 // btn.addEventListener("click", async () =>{
 //     // console.log("button was clicked");
@@ -54,4 +24,39 @@ async function  getImage(){
 //     return "No fact found"
 //   }
 // }
-console.log(getImage());
+// console.log(getFact());
+
+
+// -------------------------------------------------------------------------------
+
+// dog api's
+
+// let url2  = "https://dog.ceo/api/breeds/image/random";
+
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", async () =>{
+//     // console.log("button was clicked");
+//     let link = await getImage();
+//     // console.log(link);
+    
+//     let img = document.querySelector("#result");
+//     img.setAttribute("src", link);
+
+//     // let p = document.querySelector("#result");
+//     // p.innerText = ;
+
+// });
+
+
+// // using Axios 
+// async function  getImage(){
+//   try{
+//     let res = await axios.get(url2);
+//     // let data = await res.json();/*we do not want this line because it diretclty convert the json to java object */
+//     // return res.data.fact;
+//     return res.data.message;      
+//   }catch(error){
+//     // console.log("Error",error); 
+//     return "No fact found"
+//   }
+// }
