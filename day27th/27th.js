c4f520426a402d61f7b19acf10e8a7ba943e3089
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-11-12 11:19:25 +0530

    dog api fetched

--- day27th/27th.js
+++ day27th/27th.js
@@ -1,28 +1,57 @@
 
-let url = "https://catfact.ninja/fact";
+// let url = "https://catfact.ninja/fact";
+let url2  = "https://dog.ceo/api/breeds/image/random";
 
 let btn = document.querySelector("button");
 btn.addEventListener("click", async () =>{
     // console.log("button was clicked");
-    let fact = await getfact();
-    // console.log(fact);
+    let link = await getImage();
+    // console.log(link);
+    
+    let img = document.querySelector("#result");
+    img.setAttribute("src", link);
 
-    let p = document.querySelector("#result");
-    p.innerText = fact;
+    // let p = document.querySelector("#result");
+    // p.innerText = ;
 
 });
 
 
 // using Axios 
-async function  getfact(){
+async function  getImage(){
   try{
-    let res = await axios.get(url);
+    let res = await axios.get(url2);
     // let data = await res.json();/*we do not want this line because it diretclty convert the json to java object */
-    return res.data.fact;
-      
+    // return res.data.fact;
+    return res.data.message;      
   }catch(error){
     // console.log("Error",error); 
     return "No fact found"
   }
 }
-// console.log(getfact());
+
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", async () =>{
+//     // console.log("button was clicked");
+//     let fact = await getfact();
+//     // console.log(fact);
+
+//     let p = document.querySelector("#result");
+//     p.innerText = fact;
+
+// });
+
+
+// // using Axios 
+// async function  getfact(){
+//   try{
+//     let res = await axios.get(url);
+//     // let data = await res.json();/*we do not want this line because it diretclty convert the json to java object */
+//     return res.data.fact;
+      
+//   }catch(error){
+//     // console.log("Error",error); 
+//     return "No fact found"
+//   }
+// }
+console.log(getImage());
